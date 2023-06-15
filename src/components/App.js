4280144870bc366b42cba2fd045288b37ac067d1
Ifeanyi Ani <ifeanyiani155@gmail.com>
2023-06-15 11:59:03 +0100

    initialize our app and add App and Search component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  //api key: AIzaSyAIF0PCbcje9mKHkaFebmdwPLUgFBZ5DJ8
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    )
+  }
+}
+export default App;
